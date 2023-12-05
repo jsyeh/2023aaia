b90b8e1f103540a072448f811e48c04c33ab1a6e
Jeng-Sheng Yeh 葉正聖 <jsyeh@mail.mcu.edu.tw>
2023-12-05 12:15:40 +0800

    week13

---
+++ week13/week13-4.py
@@ -0,0 +1,10 @@
+# LeetCode 191. Number of 1 Bits
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        ans = 0 # 迴圈前面 ans=0
+
+        while n>0: # 要把 n 剝光光
+            ans += n%2 # 剝下來的皮
+            n = n // 2 # n 剝完就變小了
+
+        return ans # 迴圈後面 ans 拿來用
